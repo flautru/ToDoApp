17b57effb19cd19b3bfce3a8e31e09c2416745e3
flautru <flautru@gmail.com>
2025-06-27 12:02:01 +0200

    Add header and footer components, improve layout and validation

--- back/ToDoApp/src/main/java/com/fabien/ToDoApp/model/Task.java
+++ back/ToDoApp/src/main/java/com/fabien/ToDoApp/model/Task.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,7 +19,11 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(max = 100)
     private String label;
+    @Size(max = 500)
     private String description;
     private boolean completed;
 
